0fe106e814c594b34eeae79040b1351df3973783
homer <homer@DESKTOP-OOM2I4V>
2023-02-26 11:32:24 -0500

    Agregar archivos de proyecto.

---
+++ DIGITAL/Models/ViewModels/EmpleadoVM.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace DIGITAL.Models.ViewModels {
+
+    public class EmpleadoVM {
+
+        public Empleado oEmpleado { get; set; }
+        public List<SelectListItem> oListaCargo { get; set; }
+
+    }
+}
