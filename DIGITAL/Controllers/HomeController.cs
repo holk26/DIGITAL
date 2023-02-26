f2806124ae0d29af7a5741417900250f833a3267
Holk CA <homero97c@hotmail.com>
2023-02-26 13:33:38 -0500

    Formulario OK

--- DIGITAL/Controllers/HomeController.cs
+++ DIGITAL/Controllers/HomeController.cs
@@ -4,6 +4,8 @@ using DIGITAL.Models;
 using DIGITAL.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using NPOI.XSSF.UserModel; //para trabajar con archivos xlsx
+
 
 namespace DIGITAL.Controllers {
     public class HomeController : Controller {
@@ -14,9 +16,24 @@ namespace DIGITAL.Controllers {
             _DBcontext = context;
         }
 
-        public IActionResult Index() {
-            List<Empleado> lista = _DBcontext.Empleados.Include(c => c.oCargo).ToList();
-            return View(lista);
+        public IActionResult Index(int idEmpleadoX = 0) {
+            //List<Empleado> lista = _DBcontext.Empleados.Include(c => c.oCargo).ToList();
+            //return View(lista);
+
+            if (idEmpleadoX == 0) {
+                
+                var clientes = from c in _DBcontext.Empleados.Include(c => c.oCargo)
+                               select c;
+                return View(clientes.ToList());
+
+            } else {
+                var clientes = from c in _DBcontext.Empleados.Include(c => c.oCargo)
+                               where c.IdEmpleado == idEmpleadoX
+                               select c;
+
+                return View(clientes.ToList());
+            }
+            
         }
 
         [HttpGet]
@@ -69,5 +86,43 @@ namespace DIGITAL.Controllers {
 
             return RedirectToAction("Index", "Home");
         }
+
+
+
+        public ActionResult GenerateExcel() {
+            //crear libro de trabajo
+            XSSFWorkbook workbook = new XSSFWorkbook();
+
+            //crear hoja de trabajo
+            XSSFSheet sheet = (XSSFSheet)workbook.CreateSheet("Empleados");
+
+            //crear fila de encabezado
+            XSSFRow headerRow = (XSSFRow)sheet.CreateRow(0);
+            headerRow.CreateCell(0).SetCellValue("Nombre completo");
+            headerRow.CreateCell(1).SetCellValue("Correo");
+            headerRow.CreateCell(2).SetCellValue("Telefono");
+            headerRow.CreateCell(3).SetCellValue("Cargo");
+
+            List<Empleado> empleados = _DBcontext.Empleados.Include(c => c.oCargo).ToList();
+            
+            int rowNum = 1;
+
+            foreach (Empleado empleado in empleados) {
+                XSSFRow row = (XSSFRow)sheet.CreateRow(rowNum++);
+                row.CreateCell(0).SetCellValue(empleado.NombreCompleto);
+                row.CreateCell(1).SetCellValue(empleado.Correo);
+                row.CreateCell(2).SetCellValue(empleado.Telefono);
+                row.CreateCell(3).SetCellValue(empleado.oCargo?.Descripcion ?? "N/A");
+            }
+
+            //escribir el libro de trabajo en un MemoryStream
+            MemoryStream stream = new MemoryStream();
+            workbook.Write(stream);
+            stream.Close();
+
+            //regresar el archivo Excel como un archivo descargable
+            return File(stream.ToArray(), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "Tareas.xlsx");
+        }
+
     }
 }
